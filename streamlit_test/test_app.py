c13abf19fe62b6a4990393e0d7040b2631ea1c1b
ale94mleon <ale94mleon@gmail.com>
2023-02-28 13:50:07 +0100

    adding some docs

--- streamlit_test/test_app.py
+++ streamlit_test/test_app.py
@@ -4,14 +4,63 @@ from rdkit.Chem import Descriptors
 from rdkit.Chem import Draw
 from io import BytesIO
 import streamlit as st
-st.title('✍️ molecules with JSME in Streamlit 🤩')
+st.title('✍️ molecules with JSME in Streamlit')
+
+with st.expander('**Code**'):
+    st.markdown("""You must have a `requirements.txt` in the directory of the Streamlit App
+
+```bash
+streamlit
+rdkit
+StreamJSME
+```
+
+And also `packages.txt` at the root directory of your GitHub repo
+
+```bash
+libxrender1
+```
+
+Finally the python code: 
+
+``` python
+from StreamJSME import StreamJSME
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem import Draw
+from io import BytesIO
+import streamlit as st
+st.title('✍️ molecules with JSME in Streamlit')
+
 # Create a first plot with an input SMILES, by default smiles = 'C'
 update_smiles = StreamJSME(smiles='CCC')
 
 st.subheader('Using the draw molecule inside RDKit')
 st.write(f"New SMILES = {update_smiles}")
 mol = Chem.MolFromSmiles(update_smiles)
-st.write(f"MolLogP = {Descriptors.MolLogP(mol)}\n\nTPSA = {Descriptors.TPSA(mol)}")
+st.write(f"MolLogP = {Descriptors.MolLogP(mol)}")
+st.write(f"TPSA = {Descriptors.TPSA(mol)}")
+
+st.subheader('Getting the RDKit image')
+img = Draw.MolToImage(mol)
+bio = BytesIO()
+img.save(bio, format='png')
+st.image(img)
+```
+
+Check [StreamJSME GitHub Repo](https://github.com/ale94mleon/StreamJSME).
+""")
+
+
+
+# Create a first plot with an input SMILES, by default smiles = 'C'
+update_smiles = StreamJSME(smiles='CCC')
+
+st.subheader('Using the draw molecule inside RDKit')
+st.write(f"New SMILES = {update_smiles}")
+mol = Chem.MolFromSmiles(update_smiles)
+st.write(f"MolLogP = {Descriptors.MolLogP(mol)}")
+st.write(f"TPSA = {Descriptors.TPSA(mol)}")
 
 st.subheader('Getting the RDKit image')
 img = Draw.MolToImage(mol)
